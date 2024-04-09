e9fd4f0f8a2a4fd75e059780d759a26128871997
Ariadnavargas <at.vargasc1@uniandes.edu.co>
2024-04-09 03:07:07 +0000

    tarea 5

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 import {Aprendiz, NivelEducativo } from "./aprendiz.js";
+import { Curso } from "./curso.js";
 
-export const ap= new Aprendiz("Laura", "Rodriguez Sierra", "avatar.png", 20, NivelEducativo.UNIVERSITARIO);
+let cursos=[new Curso("EDA", 72,100, true, 2022), 
+new Curso("Abstracta_1", 64, 100, true, 2023), 
+new Curso("Analisis", 64,100,true,2023),
+new Curso("Vectorial",72,100,false,2023)]
 
+export const ap= new Aprendiz("Laura", "Rodriguez Sierra", "avatar.png", 20, NivelEducativo.UNIVERSITARIO, cursos);
+console.log(ap.cursos);
 let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+let estadisticasTable: HTMLElement= document.getElementById("estadisticas")!;
 mostrarDatoAprendiz(ap);
+mostrarEstadisticas(ap);
 
 function mostrarDatoAprendiz(aprendiz: Aprendiz):void{
     let tbodyAprendiz = document.createElement("tbody");
@@ -12,10 +20,14 @@ function mostrarDatoAprendiz(aprendiz: Aprendiz):void{
      <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
      <tr><td>Nivel Educativo:</td><td>${aprendiz.nivelEducativos}</td></tr>
      <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`
-     aprendizTable.appendChild(tbodyAprendiz);
+     aprendizTable.appendChild(tbodyAprendiz);   
+}
 
-    
-    
-    
+
+function mostrarEstadisticas(aprendiz:Aprendiz):void{
+    let numeroCertificados:number=aprendiz.darCursosCertificados();
+    let trElement:HTMLElement = document.createElement("tr");
+    trElement.innerHTML=`<td><b>Cursos certificados</td><td>${numeroCertificados}</td>`;
+    estadisticasTable.appendChild(trElement);
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,23 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
-export var ap = new Aprendiz("Laura", "Rodriguez Sierra", "avatar.png", 20, NivelEducativo.UNIVERSITARIO);
+import { Curso } from "./curso.js";
+var cursos = [new Curso("EDA", 72, 100, true, 2022),
+    new Curso("Abstracta_1", 64, 100, true, 2023),
+    new Curso("Analisis", 64, 100, true, 2023),
+    new Curso("Vectorial", 72, 100, false, 2023)];
+export var ap = new Aprendiz("Laura", "Rodriguez Sierra", "avatar.png", 20, NivelEducativo.UNIVERSITARIO, cursos);
+console.log(ap.cursos);
 var aprendizTable = document.getElementById("aprendiz");
+var estadisticasTable = document.getElementById("estadisticas");
 mostrarDatoAprendiz(ap);
+mostrarEstadisticas(ap);
 function mostrarDatoAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement("tbody");
     tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./" + aprendiz.avatar + "\" height= 100></td></tr>\n    <tr><td>Nombres</td><td>" + aprendiz.nombres + "</td></tr>\n     <tr><td>Apellidos:</td><td>" + aprendiz.apellidos + "</td></tr>\n     <tr><td>Nivel Educativo:</td><td>" + aprendiz.nivelEducativos + "</td></tr>\n     <tr><td>Edad:</td><td>" + aprendiz.edad + "</td></tr>";
     aprendizTable.appendChild(tbodyAprendiz);
+}
+function mostrarEstadisticas(aprendiz) {
+    var numeroCertificados = aprendiz.darCursosCertificados();
+    var trElement = document.createElement("tr");
+    trElement.innerHTML = "<td><b>Cursos certificados</td><td>" + numeroCertificados + "</td>";
+    estadisticasTable.appendChild(trElement);
 }
